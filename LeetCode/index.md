80ad442950599370f58cf7ee81a5ce65a0d720b9
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2024-06-21 19:36:47 -0400

    Quartz sync: Jun 21, 2024, 7:36 PM

---
+++ LeetCode/index.md
@@ -0,0 +1,15 @@
+---
+title: Welcome
+---
+
+Hi 👋 My name is Aleksandr and I built this website to collect solutions to [Neetcode 150](https://neetcode.io/practice) problems! My goal is to outline solutions to all problems. By doing this I am improving my ability to explain algorithmic solutions and creating a valuable resource for others preparing for coding interviews or enhancing their problem-solving skills.
+
+There are a few rules I set to myself:
+- All problems should have `multiple solutions` (when possible)
+- Each solution should include `time complexity` and `space complexity`
+- All solutions should be broken into `puzzle pieces` - small, manageable parts that make it easier to understand the overall approach
+- And...
+
+```python
+print("All problems should we solved in `Python` 🦑✨")
+```
---
+++ quartz.config.ts
@@ -0,0 +1,91 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+/**
+ * Quartz 4.0 Configuration
+ *
+ * See https://quartz.jzhao.xyz/configuration for more information.
+ */
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "✨ Neetcode 150",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: {
+      provider: "plausible",
+    },
+    locale: "en-US",
+    baseUrl: "quartz.jzhao.xyz",
+    ignorePatterns: ["private", "templates", ".obsidian"],
+    defaultDateType: "created",
+    theme: {
+      fontOrigin: "googleFonts",
+      cdnCaching: true,
+      typography: {
+        header: "Rubik",
+        body: "JetBrains Mono",
+        code: "JetBrains Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#fcfcfe",
+          lightgray: "#e5e5e5",
+          gray: "#b8b8b8",
+          darkgray: "#33396b",
+          dark: "#2a4184",
+          secondary: "#284b63",
+          tertiary: "#e75590",
+          highlight: "rgba(143, 159, 169, 0.15)",
+        },
+        darkMode: {
+          light: "#161618",
+          lightgray: "#393639",
+          gray: "#646464",
+          darkgray: "#d4d4d4",
+          dark: "#ebebec",
+          secondary: "#7b97aa",
+          tertiary: "#e75590",
+          highlight: "rgba(143, 159, 169, 0.15)",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "filesystem"],
+      }),
+      Plugin.SyntaxHighlighting({
+        theme: {
+          light: "github-light",
+          dark: "github-dark",
+        },
+        keepBackground: false,
+      }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.TableOfContents(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Description(),
+      Plugin.Latex({ renderEngine: "katex" }),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources(),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.NotFoundPage(),
+    ],
+  },
+}
+
+export default config
